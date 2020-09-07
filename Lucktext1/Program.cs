b42865c2da36c67e00c22ceacfd6e67501d1d8f9
christos <potamitischristos@gmail.com>
2020-09-07 10:08:35 +0200

    Lucktext

---
+++ Lucktext1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Lucktext
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            string age;
+            string color;
+
+            Console.WriteLine("Hej, vad heter du?");
+
+            name = Console.ReadLine();
+
+            Console.WriteLine(name + " Hur gammal är du?");
+
+            age = Console.ReadLine();
+
+            Console.WriteLine(name + " vad är din favorit färg?");
+
+            color = Console.ReadLine();
+
+            Console.WriteLine("Du heter " + name + " och är " + age + " och din favoritfärg är " + color);
+
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
